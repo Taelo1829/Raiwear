85ce548d1e8b460729cb597c4a30aed51f79e2ae
Taelo Seholoba <tseholoba2@gmail.com>
2024-01-14 19:21:19 +0200

    footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,10 +7,15 @@ const Footer = () => {
     let secondColumnTitle: string[] = ["Email Address", "raiwear@outlook.co.za", "Instagram", "raiwear.studio"];
     return (
         <div className='bg-orange-custom p-10 flex justify-center'>
-            <div className='text-red-custom text-lg container flex justify-between'>
+            <div className='text-red-custom text-lg container flex sm:justify-center md:justify-between '>
                 <FooterColumn titles={firstColumnTitles} mainHeading={"Shop"} />
-                <FooterColumn titles={firstColumnTitles} mainHeading={"Info"} />
-                <FooterColumn titles={secondColumnTitle} mainHeading={"Contact Us"} />
+                <div className='hidden md:inline-block'>
+                    <FooterColumn titles={firstColumnTitles} mainHeading={"Info"} />
+                </div>
+                <div className='hidden md:inline-block'>
+                    <FooterColumn titles={secondColumnTitle} mainHeading={"Contact Us"} />
+                </div>
+
             </div>
         </div>
     )
