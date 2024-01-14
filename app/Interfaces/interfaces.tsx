404490be6c90933f24d9cc2d9459641675e3dc06
Taelo Seholoba <tseholoba2@gmail.com>
2024-01-14 18:07:40 +0200

    refractored code

--- app/Interfaces/interfaces.tsx
+++ app/Interfaces/interfaces.tsx
@@ -2,16 +2,6 @@ export interface BannerInterface {
     heading: string
 }
 
-export interface ShoppingListInterface {
-    index: number
-    item: {
-        title: string,
-        image: string,
-        price: string,
-        available: boolean
-    }
-}
-
 export interface CategoryItemInterface {
     title: string
     image: string
