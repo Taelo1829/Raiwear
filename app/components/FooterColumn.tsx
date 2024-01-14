ff2c66a1ccaf206dd8a4b71b08fe0a39c572afb8
Taelo Seholoba <tseholoba2@gmail.com>
2024-01-14 17:40:09 +0200

    worked on footer

--- app/components/FooterColumn.tsx
+++ app/components/FooterColumn.tsx
@@ -4,7 +4,7 @@ import { FooterColumnInterface } from '../Interfaces/interfaces'
 
 const FooterColumn: React.FC<FooterColumnInterface> = ({ titles, mainHeading }) => {
     return (
-        <div className='scale-75'>
+        <div>
             <h1 className='text-xl'>{mainHeading}</h1>
             {titles.map((title: string, index: number) => {
                 return <FooterItem title={title} index={index} />
