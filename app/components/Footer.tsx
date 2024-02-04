b650b0f9a1e760d0732102c4ecf6bcbcc9d2631c
Taelo Seholoba <tseholoba2@gmail.com>
2024-02-04 18:24:07 +0200

    removed stuff from footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import FooterItem from './FooterItem'
-import FooterColumn from './FooterColumn'
 
 const Footer = () => {
-    let firstColumnTitles: string[] = ["Search", "Contact Us", "Term Of Use", "Privacy Policy", "Terms Of Service"];
-    let secondColumnTitle: any[] = [<b>Email Address</b>, "raiwemarketing@outlook.com", <b>Instagram</b>, "raiwear.studio", <b>Whatsapp</b>, "+27 73 376 7873"];
     return (
         <div className='flex justify-center font-mono pb-10'>
             <div className='container flex justify-around'>
