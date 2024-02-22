076d1a39bc2716d4528d4a5a814dad4c7053e476
Taelo Seholoba <tseholoba2@gmail.com>
2024-02-23 00:01:07 +0200

    worked on header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Search from './Search'
 const User = JSON.parse(localStorage.getItem("currentUser") || "{}");
-console.log(User)
 const Header = () => {
     const [hidden, setHidden] = useState("hidden")
     const [width, setWidth] = useState("w-0")
@@ -37,7 +36,7 @@ const Header = () => {
             <div className='flex flex-col  items-center fixed w-screen pb-5 md:h-52 border-b border-1 border-orange-custom z-10 bg-white '>
                 <div className='container pt-5 '>
                     <div className='flex justify-between items-center'>
-                        <div className=' flex-1 px-5 md:inline-block cursor-pointer' data-modal-target="modal"><i className='fa fa-bars md:text-4xl sm:text-2xl' onClick={updateMenu}></i></div>
+                        <div className=' flex-1 px-5 md:inline-block cursor-pointer' data-modal-target="modal"><Link href={"/collection"}><i className='fa fa-bars md:text-4xl sm:text-2xl' ></i></Link></div>
                         <Link className='flex-1 flex justify-center' href={"/"}><Image alt="Raiwear Logo" src="/img/4.png" className='sm:w-96 md:w-52' width={100} height={100} /></Link>
                         <div className='flex-1 px-5'>
                             <div className='float-end'>
@@ -45,6 +44,18 @@ const Header = () => {
                                 <span>Shopping Bag ({0})</span>
                             </div>
                         </div>
+                    </div>
+                    <div className='flex justify-center my-2'>
+                      <Link href={"/collection"} className='hover:underline mx-5 font-mono'>collection</Link>  
+                      <Link href={"/about-us"} className='hover:underline mx-5 font-mono'>about us</Link>  
+                      <Link href={"/contact-us"} className='hover:underline mx-5 font-mono'>contact Us</Link>  
+                      <Link href={"/blog"} className='hover:underline mx-5 font-mono'>blog</Link>  
+                    <select onChange={(e)=>window.location.href = e.target.value}>
+                        <option>admin</option>
+                        <option value={"/admin-categories"}>categories</option>
+                        <option value={"/clothes"}>clothes</option>
+                        <option value={"/specials"}>sales</option>
+                    </select>
                     </div>
                     {hideSearch ? "" : <Search />}
                     <Menu toggleMenu={updateMenu} hidden={hidden} width={width} />
