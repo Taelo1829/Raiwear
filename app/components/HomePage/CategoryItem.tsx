b1f2ccd13d8bd1863d04312dc6525c8bdd49db98
Taelo Seholoba <tseholoba2@gmail.com>
2024-04-23 15:40:56 +0200

    worked on shopping category

--- app/components/HomePage/CategoryItem.tsx
+++ app/components/HomePage/CategoryItem.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const CategoryItem: React.FC<CategoryItemInterface> = ({ title, image }) => {
     return (
-        <div className='m-7 flex flex-col'>
-            <Image alt="bags" src={image} className='h-full' height={200} width={200} />
+        <div className='px-7 flex flex-col'>
+            <div className='flex bg-blue-100 w-48 h-48 justify-center items-center bg-cover bg-center' style={image?{backgroundImage:`url(${image})`}:{}}></div>
             <div className='flex justify-between items-center py-2 text-red-custom'>{title} <i className='fa fa-arrow-right mx-1'></i></div>
         </div>
     )
--- app/components/HomePage/ShoppingCategory.tsx
+++ app/components/HomePage/ShoppingCategory.tsx
@@ -3,15 +3,27 @@ import CategoryItem from './CategoryItem'
 import { OnSaleInterface } from '@/app/Interfaces/interfaces'
 
 const ShoppingCategory: React.FC<OnSaleInterface> = ({products}) => {
+    let collections:any[] = []
+    for(let i = 0; i < products.length; i++){
+        let index = collections.findIndex((item:any) => item.collection === products[i]?.collection)
+        if(index === -1){
+            collections.push({collection:products[i]?.collection, image:products[i]?.images && products[i]?.images[0]})
+        } else {
+            if(products[i]?.images && products[i]?.images[0]){
+                collections[index].image = products[i].images[0]
+            }
+        
+        }
+    }
+    console.log(collections)
     return (
         <div className='bg-orange-custom my-5'>
-            <div>
-                <p className='text-3xl text-red-custom p-3 text-center'>SHOP BY CATEGORY</p>
-                <div className='flex justify-center w-100 flex-wrap'>
-                    <CategoryItem title='Bags & Accessories' image="/img/stock/bags.jpg" />
-                    <CategoryItem title='Shoes' image="/img/stock/boots.jpg" />
-                    <CategoryItem title='Hats' image="/img/stock/hats.jpg" />
-                    <CategoryItem title='Shirts' image="/img/stock/shirts.jpg" />
+            <div className='h-96 overflow-x-scroll px-2'>
+                <p className='text-3xl text-red-custom p-3 text-center '>SHOP BY CATEGORY</p>
+                <div className='flex justify-center w-100'>
+                    {collections.map((item:any,index:number)=>{
+                        return <CategoryItem title={item.collection} image={item.image} />
+                    })}
                 </div>
             </div>
         </div>
